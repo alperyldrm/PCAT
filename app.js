53b2c7a9a86558d42c0936fefc0e190dc7d3b492
Alper Yildirim <alperyldrm96@gmail.com>
2024-01-13 16:32:52 +0300

    second video ejs

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
+//TEMPLATE Engine
+app.set('view engine', 'ejs');
+
 //MIDDLEWARE
 app.use(express.static('public'));
 
-const port = 3000;
-
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve('temp','index.html'));
+  res.render('index');
 });
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Sunucu belirtilen portta baslatildi : ${port}`);
