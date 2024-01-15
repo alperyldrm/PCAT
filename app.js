052e2cdda51952a5c9620fb9fa98f18b4b97db52
Alper Yildirim <alperyldrm96@gmail.com>
2024-01-15 14:42:45 +0300

    Model ve Dinamik Icerik Dersi Bitti

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//connect db
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 //TEMPLATE Engine
 app.set('view engine', 'ejs');
 
 //MIDDLEWARE
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async(req, res) => {
+  const photos = await Photo.find({})
+  res.render('index',{
+    photos
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -20,9 +30,13 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/');
+});
 
+//PORT olayi
 const port = 3000;
-
 app.listen(port, () => {
   console.log(`Sunucu belirtilen portta baslatildi : ${port}`);
 });
